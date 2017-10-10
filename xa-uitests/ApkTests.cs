0bdc67860bae819d4e67edbcbc72162a4e364f3c
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-10-10 16:03:25 -0500

    Like tests? let's put tests in your tests
    Context: https://github.com/xamarin/xamarin-android/compare/master...jonathanpeppers:wip-testcloud
    
    NUnit has a feature to express parameterized test cases dynamically. So, we execute the NUnit tests via UITest and pass the XML results back through a backdoor method that I added to XA's copy of NUnitLite.
    
    TODO:
    - I don't have the proper `result` values parsed and doing the right thing
    - I've not been able to find the docs for the NUnit2 xml format, might need to just find the source
    - We might be able to better map the XML attribute than dumping them all in properties
    - I need to try this on Test Cloud (local works)

--- xa-uitests/ApkTests.cs
+++ xa-uitests/ApkTests.cs
@@ -1,29 +1,60 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
 using NUnit.Framework;
 using Xamarin.UITest;
-using Xamarin.UITest.Android;
 
-namespace Xamarin.Android.UITests
+[TestFixture]
+public class TestCloud
 {
-	[TestFixture]
-	public class ApkTests
+	static IEnumerable Results
 	{
-		AndroidApp app;
-
-		[SetUp]
-		public void SetUp()
+		get
 		{
-			app = ConfigureApp.Android
-				.ApkFile("/Users/jonathanpeppers/Desktop/Git/xamarin-android/bin/TestDebug/Xamarin.Android.JcwGen_Tests.apk")
-				.StartApp();
+			var app = ConfigureApp.Android
+								  .ApkFile("/Users/jonathanpeppers/Desktop/Git/xamarin-android/bin/TestDebug/Xamarin.Android.JcwGen_Tests.apk")
+								  .StartApp();
+
+			string result = app.Invoke("RunTests").ToString();
+			Console.WriteLine(result);
+
+			var xml = new XmlDocument();
+			xml.LoadXml(result);
+
+			var results = new List<TestCaseData>();
+			foreach (XmlElement node in xml.SelectNodes("//test-case"))
+			{
+				string fullName = node.Attributes["name"].Value;
+				string name = fullName.Split(',').Last().Trim();
+				string testResult = node.Attributes["result"].Value;
+				var data = new TestCaseData(testResult)
+					.SetName(name)
+					.SetDescription(fullName);
+				foreach (XmlAttribute attribute in node.Attributes)
+				{
+					data.SetProperty(attribute.Name, attribute.Value);
+				}
+				results.Add(data);
+			}
+			return results;
 		}
+	}
 
-		[Test]
-		public void RunAllTests()
+	[Test, TestCaseSource("Results")]
+	public void Test(string result)
+	{
+		switch (result)
 		{
-			app.Tap(q => q.Marked("Run Tests"));
-
-			app.WaitForElement(q => q.Marked("Passed"));
+			case "Success":
+				break;
+			case "Failure":
+				Assert.Fail();
+				break;
+			default:
+				Assert.Inconclusive("Unknown result: " + result);
+				break;
 		}
 	}
 }
