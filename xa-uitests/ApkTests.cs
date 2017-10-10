cd25c6016f7804a440537fd14da52d1a3b242fe4
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-10-10 10:52:40 -0500

    Initial project that works
    Basically can just click "Run Tests" and wait for "Passed"

---
+++ xa-uitests/ApkTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+
+namespace Xamarin.Android.UITests
+{
+	[TestFixture]
+	public class ApkTests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void SetUp()
+		{
+			app = ConfigureApp.Android
+				.ApkFile("/Users/jonathanpeppers/Desktop/Git/xamarin-android/bin/TestDebug/Xamarin.Android.JcwGen_Tests.apk")
+				.StartApp();
+		}
+
+		[Test]
+		public void RunAllTests()
+		{
+			app.Tap(q => q.Marked("Run Tests"));
+
+			app.WaitForElement(q => q.Marked("Passed"));
+		}
+	}
+}
