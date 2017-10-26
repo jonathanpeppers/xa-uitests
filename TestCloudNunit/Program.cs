0668640b8ef260f823709a93bb5c322a8879eec4
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-10-26 10:48:52 -0500

    Mainly just cleanup code
    Mixed tabs & spaces, etc.

--- TestCloudNunit/Program.cs
+++ TestCloudNunit/Program.cs
@@ -7,16 +7,16 @@ namespace TestCloudNunit
 	{
 		static void Main(string[] args)
 		{
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Usage InputTestResults.xml OutputXmlName.xml");
-                return;
-            }
+			if (args.Length != 2)
+			{
+				Console.WriteLine("Usage InputTestResults.xml OutputXmlName.xml");
+				return;
+			}
 			Console.WriteLine("Hello World!");
 			var parser = new NUnitXmlParser();
 			var results = parser.GetTestResultXml(args[0]);
-            var outName = Path.GetFileNameWithoutExtension(args[1]);
-            results.ForEach(x => File.WriteAllText($"{outName}_{x.device}.xml", x.xml));
+			var outName = Path.GetFileNameWithoutExtension(args[1]);
+			results.ForEach(x => File.WriteAllText($"{outName}_{x.device}.xml", x.xml));
 		}
 	}
 }
--- xa-uitests/ApkTests.cs
+++ xa-uitests/ApkTests.cs
@@ -15,55 +15,8 @@ using System.IO.Compression;
 [TestFixture]
 public class TestCloud
 {
-	//static IEnumerable Results
-	//{
-	//	get
-	//	{
-	//		var app = ConfigureApp.Android
-	//							  .ApkFile("/Users/jonathanpeppers/Desktop/Git/xamarin-android/bin/TestDebug/Xamarin.Android.JcwGen_Tests.apk")
-	//							  .StartApp();
-
-	//		string result = app.Invoke("RunTests").ToString();
-	//		//Console.WriteLine(result);
-
-	//		var xml = new XmlDocument();
-	//		xml.LoadXml(result);
-
-	//		var results = new List<TestCaseData>();
-	//		foreach (XmlElement node in xml.SelectNodes("//test-case"))
-	//		{
-	//			string fullName = node.Attributes["name"].Value;
-	//			string name = fullName.Split(',').Last().Trim();
-	//			string testResult = node.Attributes["result"].Value;
-	//			var data = new TestCaseData(testResult)
-	//				.SetName(name)
-	//				.SetDescription(fullName);
-	//			foreach (XmlAttribute attribute in node.Attributes)
-	//			{
-	//				data.SetProperty(attribute.Name, attribute.Value);
-	//			}
-	//			results.Add(data);
-	//		}
-	//		return results;
-	//	}
-	//}
-	//[Test, TestCaseSource("Results")]
-	//public void Test(string result)
-	//{
-	//	switch (result)
-	//	{
-	//		case "Success":
-	//			break;
-	//		case "Failure":
-	//			Assert.Fail();
-	//			break;
-	//		default:
-	//			Assert.Inconclusive("Unknown result: " + result);
-	//			break;
-	//	}
-	//}
-
 	AndroidApp app;
+
 	[SetUp]
 	public void BeforeEachTest()
 	{
@@ -71,39 +24,25 @@ public class TestCloud
 						  .ApkFile("/Users/clancey/Projects/xamarin-android/bin/TestDebug/Xamarin.Android.Bcl_Tests.apk").WaitTimes(new WaitTime())
 						  .StartApp();
 	}
+
 	[Test]
-		public void DeviceTest()
+	public void DeviceTest()
+	{
+		var started = bool.Parse(app.Invoke("RunTests").ToString());
+		bool isRunning = started;
+		while (bool.Parse(app.Invoke("IsRunning").ToString()))
 		{
-
-			var started = bool.Parse(app.Invoke("RunTests").ToString());
-			bool isRunning = started;
-			while (bool.Parse(app.Invoke("IsRunning").ToString()))
-			{
-				Task.Delay(1000).Wait();
-			}
-
-			var result = app.Invoke("TestResults").ToString();
-			//var bytes = System.Convert.FromBase64String(r);
-			//string result = null;
-			//using (MemoryStream ms = new MemoryStream(bytes))
-			//{
-			//	using (var decompressedMs = new MemoryStream())
-			//	{
-			//		using (var gzs = new GZipStream(ms,
-			//		 CompressionMode.Decompress))
-			//		{
-			//			gzs.CopyTo(decompressedMs);
-			//		}
-
-			//		result = System.Text.Encoding.UTF8.GetString(decompressedMs.ToArray());
-			//	}
-			//}
-			bool success = bool.Parse(app.Invoke("TestRunSuccess").ToString());
-			if (success)
-				Assert.Pass(result);
-			else
-				Assert.Fail(result);
+			Task.Delay(1000).Wait();
 		}
+
+		var result = app.Invoke("TestResults").ToString();
+
+		bool success = bool.Parse(app.Invoke("TestRunSuccess").ToString());
+		if (success)
+			Assert.Pass(result);
+		else
+			Assert.Fail(result);
+	}
 
 	class WaitTime : IWaitTimes
 	{
